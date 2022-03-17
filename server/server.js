a0264c25593e59288dc1e3973f8a9c2b27a30c79
Dmitriyhk <dimassik50@gmail.com>
2022-03-17 14:30:42 +0300

    install socket.io

--- server/server.js
+++ server/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const room = new Map();
-
 
 app.get('/room', (req, res) => {
   res.json(room);
 });
 
-app.listen(8888, (error) => {
+io.on('connection', socket => {
+  console.loog('user connected > ', socket)
+})
+
+server.listen(8888, (error) => {
   if (error) {
-    throw Error(error)
+    throw Error(error); 
   }
   console.log('Сервер запущен!');
 });
