073c06195bbc2e17fb1c86ea22491c8c0ecf80c5
Dmitriyhk <dimassik50@gmail.com>
2022-03-17 11:36:33 +0300

    created a request to get messages and users

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
 const express = require('express');
+
+const app = express();
+
+const room = new Map();
+
+
+app.get('/room', (req, res) => {
+  res.json(room);
+});
+
+app.listen(8888, (error) => {
+  if (error) {
+    throw Error(error)
+  }
+  console.log('Сервер запущен!');
+});
