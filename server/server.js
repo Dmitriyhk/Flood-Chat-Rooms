c661f650caf24df042e67be4a65ce826008fc382
Dmitriyhk <dimassik50@gmail.com>
2022-03-18 20:12:17 +0300

    update style for welcome page

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+app.use(express.json())
+
 const room = new Map();
 
 app.get('/room', (req, res) => {
